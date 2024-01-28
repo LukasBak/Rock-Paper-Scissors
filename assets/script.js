45bc88cf20aeaac7e41b418722412a58ae953068
LukasBak <>
2024-01-28 17:16:50 +0000

    Started working on javascript

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// Game variables //
+
+const possibleChoices = ["rock", "paper", "scissors"];
+const userDisplay = document.getElementById("userDisplay");
+const computerDisplay = document.getElementById("computerrDisplay");
+const resultsDisplay = document.getElementById("resultsDisplay");
+
+const choices = document.getElementById("choice")
+choices.forEach
