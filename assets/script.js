e58bccec7fa54f204ff2992b0dacd94874c541b9
LukasBak <>
2024-02-02 15:24:00 +0000

    Added event listeners to js

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,26 @@ const computerDisplay = document.getElementById("computerDisplay");
 const resultsDisplay = document.getElementById("resultsDisplay");
 const userChoice = document.getElementById("userChoice")
 
-
 // Random number between 0 and 2 //
-    const computerChoice = possibleChoices[Math.floor(Math.random() * 3)];
+function getComputerChoice(){
 
-    
+const computerChoice = possibleChoices[Math.floor(Math.random() * 3)];
+return computerChoice
+}
+
+// Event listeners //
+
+const rockButton = document.getElementById('rock')
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+
+rockButton.addEventListener('click',function (){
+    determineWinner('rock')
+})
+paperButton.addEventListener('click', function () {
+    determineWinner('paper');
+});
+scissorButton.addEventListener('click', function (){
+    determineWinner('scissors')
+})
+
