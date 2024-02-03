abde761aa05dd7a35203cb141a3d84f4b0675814
LukasBak <>
2024-02-03 11:23:33 +0000

    testing script

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,61 +13,55 @@ const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
 
 rockButton.addEventListener("click", function () {
-    determineWinner("rock");
+  determineWinner("rock");
+  console.log(rockButton);
 });
 paperButton.addEventListener("click", function () {
-    determineWinner("paper");
+  determineWinner("paper");
+  console.log(paperButton);
 });
 scissorsButton.addEventListener("click", function () {
-    determineWinner("scissors");
+  determineWinner("scissors");
+  console.log(scissorsButton);
 });
 
 // Random number generation between 0 and 2 //
 
 function getComputerChoice() {
-    const computerChoice = possibleChoices[Math.floor(Math.random() * 3)];
-    return computerChoice;
+  const computerChoice = possibleChoices[Math.floor(Math.random() * 3)];
+  return computerChoice;
 }
 
 // Playing game using user's choice vs randomComputerChoice //
 // @param {string} userChoice - The user's choice -- one of  rock, paper or scissors//
 function determineWinner(userChoice) {
-    let computerChoice = getComputerChoice();
-    let winner;
+  let computerChoice = getComputerChoice();
+  let winner;
 
-    if (userChoice == "rock") {
-        if (computerChoice == "paper") {
-            winner = "computer";
-        } else if (computerChoice == "scissors") {
-            winner = "user";
-        } else {
-            winner = "tie";
-        }
-    } else if (userChoice == "paper") {
-        if (computerChoice == "rock") {
-            winner = "user";
-        } else if (computerChoice == "scissors") {
-            winner = "computer";
-        } else {
-            winner = "tie";
-        }
+  if (userChoice == "rock") {
+    if (computerChoice == "paper") {
+      winner = "computer";
+    } else if (computerChoice == "scissors") {
+      winner = "user";
     } else {
-        if (userChoice == "scissors") {
-            if (computerChoice == "paper") winner = "user";
-        } else if (computerChoice == "rock") {
-            winner = "computer";
-        } else {
-            winner = "tie";
-        }
+      winner = "tie";
     }
+    if (computerChoice == "rock") {
+      winner = "user";
+    } else if (computerChoice == "scissors") {
+      winner = "computer";
+    } else {
+      winner = "tie";
+    }
+  } else {
+    if (userChoice == "scissors") {
+      if (computerChoice == "paper") winner = "user";
+    } else if (computerChoice == "rock") {
+      winner = "computer";
+    } else {
+      winner = "tie";
+    }
+  }
 }
 
-let userScoreDisplay = 0;
-let computerScoreDisplay = 0;
-let tiedScoreDisplay = 0;
-
-function userScoreDisplay() {
-    if ((winner = "user")) {
-        userScoreDisplay + 1;
-    }
-}
+console.log(getComputerChoice());
